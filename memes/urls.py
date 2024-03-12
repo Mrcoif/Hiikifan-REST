dea62beee5a09a2e5b81c203d478cd4a26a8f86b
Mrcoif <svzagarov@gmail.com>
2024-03-12 09:10:45 +0600

    nothing

--- memes/urls.py
+++ memes/urls.py
@@ -7,5 +7,5 @@ router = routers.DefaultRouter()
 router.register(r'', MemeViewSet, basename='memes')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(r'^$', include(router.urls)),
 ]
